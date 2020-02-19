e4b6ea7f13a1ba1e286cdf8a47ed7b9e8793dd14
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-02-19 10:23:23 -0300

    Modificacion

--- src/main/java/CLaseNic.java
+++ src/main/java/CLaseNic.java
@@ -3,16 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Inicio;
 
 /**
  *
  * @author nicol
  */
-public class nico {
+public class CLaseNic {
     
-    
-    public void hola(){
-    
-    }
 }
