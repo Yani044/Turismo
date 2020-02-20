a9e24135405e61264d4d3eea997d96f08c0d5910
ignacio2507 <61191805+ignacio2507@users.noreply.github.com>
2020-02-20 11:19:43 -0300

    actualida usuario

--- src/main/java/turismo/entidades/Usuario.java
+++ src/main/java/turismo/entidades/Usuario.java
@@ -1,14 +1,82 @@
 package turismo.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
+
+@Entity
 public class Usuario {
+@Id    
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String dni;
+    private String telefono;
+
+    public Usuario() {
+    }
     
-private String Id;
-private String nombre;
-private String apellido;
-private String email;
-private String dni;
+    
+
+    public Usuario (String id, String nombre, String apellido, String email, String dni, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.dni = dni;
+        this.telefono = telefono;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String Id) {
+        this.id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
     
 }
