e18c9dd5ec6c3608010e9d66ce393ad827bba0c9
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-02-22 22:14:31 -0300

    cambioDeProyecto

--- src/main/Agencia.java
+++ src/main/Agencia.java
@@ -1,16 +1,15 @@
-package turismo.proyecto;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package agencia;
 
 /**
  *
- * @author Genesis
+ * @author nicol
  */
-public class Principal {
+public class Agencia {
 
     /**
      * @param args the command line arguments
