f6b17f3b7ec1a765c040cc5b891ef6ad46cf4862
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-02-25 19:50:14 -0300

    agregar sql scripts

---
+++ db/Scripts/InsertCategorias.sql
@@ -0,0 +1,3 @@
+INSERT INTO turismooriginal.categorias (id,descripcion,nombre) VALUES (1,'Las mas importantes','Clasicas');
+INSERT INTO turismooriginal.categorias (id,descripcion,nombre) VALUES (2,'Vinos','Wine Mendoza');
+INSERT INTO turismooriginal.categorias (id,descripcion,nombre) VALUES (3,'Destreza','Aventura');
