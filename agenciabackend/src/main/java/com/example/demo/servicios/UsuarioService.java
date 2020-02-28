a602bf80395a1b6eb0aa45f7654a88a43ff42458
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-02-28 12:59:44 -0300

    autowired

--- agenciabackend/src/main/java/com/example/demo/servicios/UsuarioService.java
+++ agenciabackend/src/main/java/com/example/demo/servicios/UsuarioService.java
@@ -6,11 +6,14 @@ import javax.persistence.Persistence;
 import com.example.demo.entidades.Usuario;
 import com.example.demo.repositorios.ProductoRepositorio;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class UsuarioService {
-    
+    @Autowired
     private ProductoRepositorio productoRepositorio;
             
   
