855fa23d887f00fd2d3888fc0743204295f36969
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-03-02 09:45:59 -0300

    seguridad

--- agenciabackend/src/main/java/com/example/demo/servicios/UsuarioService.java
+++ agenciabackend/src/main/java/com/example/demo/servicios/UsuarioService.java
@@ -5,18 +5,39 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import com.example.demo.entidades.Usuario;
 import com.example.demo.repositorios.ProductoRepositorio;
+import com.example.demo.repositorios.UsuarioRepositorio;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UsuarioService {
     @Autowired
     private ProductoRepositorio productoRepositorio;
+    private UsuarioRepositorio usuarioRepositorio;
+
+    
+
+    
+    
+   
+    
+    }
             
   
     
      
-}
+
